937ead315f4091ea0e47723746f8cb1ff6d89f0d
gorain <g.orain@sdvi.fr>
2016-09-26 13:54:11 +0200

    initialisation des tests unitaire avec PHPUnit 5.5

---
+++ tests/CoreBundle/Entity/CouleurTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CoreBundle\Tests\Entity;
+
+use CoreBundle\Entity\Couleur;
+
+class CouleurTest extends \PHPUnit_Framework_TestCase
+{
+    public function testEmptyCouleur()
+    {
+        $couleur = new Couleur();
+        $this->assertNull($couleur->getId());
+        $this->assertNull($couleur->getCodeHexa());
+
+        return $couleur;
+    }
+
+    /**
+     * @depends testEmptyCouleur
+     * @param Couleur $couleur
+     */
+    public function testSetCodeHexa($couleur)
+    {
+        $couleur->setCodeHexa("#454545");
+
+        // assert
+        $this->assertEquals("#454545", $couleur->getCodeHexa());
+
+        $couleur2 = new Couleur();
+        $couleur->setCodeHexa(454545);
+
+        // assert
+        $this->assertEquals("#454545", $couleur->getCodeHexa());
+    }
+}
